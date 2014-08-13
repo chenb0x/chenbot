11c5b47c2649eadb7a5cd24c4f6872bc8c006775
chenb0x <infoinject@gmail.com>
2014-08-13 10:54:00 +0000

    Tiny edits. Mostly Comments

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ chenb0t
 chenb0t - twitter social media manager bot
 
 08/13/2014
-==========
-This bot is written for ruby 2.0. This will constantly be a work in progress. It works by loading text files into an array based on a random number generator. I suggest keeping the text files you want to load in the same directory as the script, but moreso, use absolute pathnames.
+----------
+This bot is written for ruby 2.0. This will constantly be a work in progress. 
+It works by loading text files into an array based on a random number generator. 
+I suggest keeping the text files you want to load in the same directory as the script, but moreso, use absolute pathnames.
 
 chenbot 0.9a
 1. Want to add sql functionality
--- zenbot.rb
+++ zenbot.rb
@@ -4,12 +4,6 @@ require 'twitter'
 This is the Chenb0t(http://chenb0x.net) by MasterChen(infoinject[at]gmail.com).
 It is designed to be an automated social media manager, starting with Twitter and moving outwards from there.
 This project is open source and may be used freely as long as I get an honorable mention ;-)
-
-Things To Do:
---------------
-1. Make a koan file.
-2. Make a ChangeLog for updating purposes from here on out.
-3. Package this project into its own directory for portability.
 =end
 	mc = Twitter::REST::Client.new do |config|
 	config.consumer_key        = "YOUR_CONSUMER_KEY"
@@ -24,6 +18,7 @@ tweet_array = []
 #High random for entropy?
 d = rand(1200).to_i
 
+#Load text file based on RNG. Only one array needed here.
 tweet_array = IO.readlines('/home/chen/chenbot/zensay.txt') if d % 6 == 0
 tweet_array = IO.readlines('/home/chen/chenbot/kempo.txt') if d % 6 == 1
 tweet_array = IO.readlines('/home/chen/chenbot/highlights.txt') if d % 6 == 2
@@ -31,9 +26,11 @@ tweet_array = IO.readlines('/home/chen/chenbot/quotes.txt') if d % 6 == 3
 tweet_array = IO.readlines('/home/chen/chenbot/social.txt') if d % 6 == 4
 tweet_array = IO.readlines('/home/chen/chenbot/koans.txt') if d % 6 == 5
 
+#Make the new array as big as the loaded textfile and grab a random num  based on that length
 r = rand(tweet_array.length).to_i
 mc.update(tweet_array[r] + "#chenb0t")
 
+#Autofollow users who stary following you. Optional.
 follower_ids = []
 mc.follower_ids("chenb0x").each do |id|
   follower_ids.push(id)
